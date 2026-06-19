c2361f7a7437f55b881dbc429f9966f6cc1f9134
james.lee <jamesrafe10@gmail.com>
2026-06-19 21:42:21 +0700

    feat: response folding

--- src/XraySpectra.jl
+++ src/XraySpectra.jl
@@ -3,6 +3,7 @@ module XraySpectra
 import FITSFiles
 import SpectrumBase
 import Distributions
+import LinearAlgebra: mul!
 
 using SpecialFunctions: gamma_inc_inv
 using SparseArrays
@@ -14,6 +15,7 @@ export ResponseMatrix, response_bins, channel_bins
 export response_bins_low, response_bins_high, channel_bins_low, channel_bins_high
 export response_energy, folded_energy, energy_binned_spectrum
 export AncillaryResponse, ancillary_bins, ancillary_bins_low, ancillary_bins_high, effective_area
+export combine, combine!, fold, fold!
 
 include("response.jl")
 include("ogip.jl")
